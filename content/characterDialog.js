af66acb16e1eb56cc6e8accf38d6bcd560ce71f6
L. David Baron <dbaron@dbaron.org>
2006-11-14 22:49:41 -0800

    Basic tree view working.

--- content/characterDialog.js
+++ content/characterDialog.js
@@ -1,4 +1,4 @@
-/* vim: set shiftwidth=4 tabstop=8 autoindent cindent expandtab: */
+/* vim: set shiftwidth=4 tabstop=4 autoindent cindent noexpandtab: */
 /* ***** BEGIN LICENSE BLOCK *****
  * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
@@ -35,38 +35,87 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-/*
- * An array of objects with the following properties:
- *   "char": a string containing the character
- *   "unicode": an integer containing the unicode codepoint
- *   "description" (optional, since lazily constructed): character description
- */
-var gChars;
-
 function CharacterDialogOnLoad()
 {
-    var string = window.arguments[0];
-    gChars = [];
-    for (var idx = 0; idx < string.length; ++idx) {
-        var length = 1;
-        var char_obj = {};
-        if ((string.charCodeAt(idx) & 0xFC00) == 0xD800 &&
-            idx + 1 < string.length &&
-            (string.charCodeAt(idx + 1) & 0xFC00) == 0xDC00) {
-            // Convert the surrogate pair.
-            char_obj.char = string.substring(idx, idx + 2);
-            var h = string.charCodeAt(idx) & 0x03FF;
-            var l = string.charCodeAt(idx + 1) & 0x03FF;
-            char_obj.unicode = 0x10000 + (h << 10) + l;
-            ++idx; // extra increment
-        } else {
-            char_obj.char = string.substring(idx, idx + 1);
-            char_obj.unicode = string.charCodeAt(idx);
-        }
-        gChars.push(char_obj);
-    }
+	var string = window.arguments[0];
+	/*
+	 * An array of objects with the following properties:
+	 *   "char": a string containing the character
+	 *   "unicode": an integer containing the unicode codepoint
+	 *   "description" (optional, since lazily constructed): character
+	 *                 description
+	 */
+	var char_objs = [];
 
-    // XXX WRITE ME
+	for (var idx = 0; idx < string.length; ++idx) {
+		var length = 1;
+		var char_obj = {};
+		if ((string.charCodeAt(idx) & 0xFC00) == 0xD800 &&
+			idx + 1 < string.length &&
+			(string.charCodeAt(idx + 1) & 0xFC00) == 0xDC00) {
+			// Convert the surrogate pair.
+			char_obj.char = string.substring(idx, idx + 2);
+			var h = string.charCodeAt(idx) & 0x03FF;
+			var l = string.charCodeAt(idx + 1) & 0x03FF;
+			char_obj.unicode = 0x10000 + (h << 10) + l;
+			++idx; // extra increment
+		} else {
+			char_obj.char = string.substring(idx, idx + 1);
+			char_obj.unicode = string.charCodeAt(idx);
+		}
+		char_objs.push(char_obj);
+	}
+
+	var tree_view = {
+		rowCount: char_objs.length,
+		selection: null,
+		getRowProperties: function(index, properties) {},
+		getCellProperties: function(row, col, properties) {},
+		getColumnProperties: function(col, properties) {},
+		isContainer: function(index) { return false; },
+		isContainerOpen: function(index) { return false; },
+		isContainerEmpty: function(index) { return false; },
+		isSeparator: function(index) { return false; },
+		isSorted: function() { return false; },
+		canDrop: function(index, orientation) { return false; },
+		drop: function(index, orientation) { throw Components.results.NS_ERROR_UNEXPECTED; },
+		getParentIndex: function(rowIndex) { return -1; },
+		hasNextSibling: function(rowIndex, afterIndex) { /* XXX ??? */ return false; },
+		getLevel: function(index) { return 0; },
+		getImageSrc: function(row, col) { return ""; },
+		getCellValue: function(row, col) { throw Components.results.NS_ERROR_UNEXPECTED; },
+		getCellText: function(row, col) {
+			var char_obj = char_objs[row];
+			if (col.id == "chars:char")
+				return char_obj.char;
+			if (col.id == "chars:unicode")
+				return "U+" + char_obj.unicode.toString(16).toUpperCase();
+			if (col.id == "chars:description")
+				return ""; // XXX WRITE ME!
+			throw Components.results.NS_ERROR_UNEXPECTED;
+		},
+		setTree: function(tree) { this.treebox = tree; },
+		toggleOpenState: function(index) { throw Components.results.NS_ERROR_UNEXPECTED; },
+		cycleHeader: function(col) {},
+		selectionChanged: function() {},
+		cycleCell: function(row, col) {},
+		isEditable: function(row, col) { return false; },
+		isSelectable: function(row, col) { return false; },
+		setCellValue: function(row, col) { throw Components.results.NS_ERROR_UNEXPECTED; },
+		setCellText: function(row, col) { throw Components.results.NS_ERROR_UNEXPECTED; },
+		performAction: function(action) {},
+		performActionOnRow: function(action, row) {},
+		performActionOnCell: function(action, row, col) {},
+
+		QueryInterface: function(iid) {
+			if (iid.equals(Components.interfaces.nsISupports) ||
+				iid.equals(Components.interfaces.nsITreeView))
+				return this;
+			throw Components.results.NS_ERROR_NO_INTERFACE;
+		}
+	};
+
+	document.getElementById("chars-tree").view = tree_view;
 }
 
 function CharacterDialogOnUnload()
